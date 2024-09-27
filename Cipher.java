04dbf1d03488385277919e4ea3bb6eca38020c99
Zeynep <avcizeynepu@gmail.com>
2024-09-27 18:42:30 +0300

    Merged correctly

--- Cipher.java
+++ Cipher.java
@@ -15,8 +15,12 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
+<<<<<<< HEAD
             // append the encrypted version of the char to the output string
             outputString += replaceChar(inputString.charAt(i), true);
+=======
+
+>>>>>>> origin/main
         }
 
         return outputString;
@@ -27,6 +31,7 @@ public class Cipher
         // output string will be collected in this variable, one char at a time
         String outputString = "";
         
+<<<<<<< HEAD
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)
         {
@@ -34,6 +39,10 @@ public class Cipher
             outputString += replaceChar(inputString.charAt(i), false);
         }
 
+=======
+        replaceChar('a',true);
+        
+>>>>>>> origin/main
         return outputString;
     }
 
@@ -48,7 +57,11 @@ public class Cipher
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
+<<<<<<< HEAD
                     return CIPHER_ALPHABET.charAt(i);
+=======
+
+>>>>>>> origin/main
                 }
             }
         }
